ed6f3138d4a0a1f40c51ee8addfaae7bb2a7d50b
oolayinkaadio <oolayinkaadio2gmail.com>
2022-04-26 09:44:26 +0100

    added routes to the application

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
 
-import { router } from './routes';
-import { errorResponse, statusCodes, successResponse } from './utils';
+const router = require('./routes/index');
+const { errorResponse, statusCodes, successResponse, messages } = require('./utils');
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan(':method :url :response-time'));
 
 const port = process.env.PORT || 4200;
 
-app.use('/api/v1', router);
+app.use('/api/v1/', router);
 
-app.get('/api/v1', (req, res) => {
-  successResponse(res, 200, messages.welcome);
+app.get('/api/v1/', (req, res) => {
+  successResponse(res, statusCodes.success, messages.welcome);
 });
 
 app.all('*', (req, res) => errorResponse(res, statusCodes.created, messages.notFound));
