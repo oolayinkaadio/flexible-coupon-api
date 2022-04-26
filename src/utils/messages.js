ad57b173bb5f1f855536fc81cdf0390260f4e340
oolayinkaadio <oolayinkaadio2gmail.com>
2022-04-26 10:09:58 +0100

    added messages utilities data

---
+++ src/utils/messages.js
@@ -0,0 +1,12 @@
+const messages = {
+  welcome: 'welcome on board',
+  created: 'resource successfully created',
+  success: 'request was successful',
+  notFound: 'resource not available',
+  badRequest: 'something bad happened,try again with a valid input',
+  conflict: 'resource already exists!',
+  validationError: 'validation error, check payload and try again',
+  deleted: 'resource successfully deleted',
+};
+
+module.exports = { messages };
