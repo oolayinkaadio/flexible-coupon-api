b3795862b3f5a2715cc70c74970ff1cb7578835e
oolayinkaadio <oolayinkaadio2gmail.com>
2022-04-27 03:45:14 +0100

    final fix 3

--- src/utils/calculateCartTotalPrice.js
+++ src/utils/calculateCartTotalPrice.js
@@ -2,7 +2,7 @@ function calculateCartTotalPrice(cartProducts) {
   let totalPrice = 0;
 
   if (cartProducts && cartProducts.length > 0) {
-    totalPrice = cartProducts.reduce((a, b) => {return a.price + b.price;}, 0);
+    totalPrice = cartProducts.reduce(((a, b) => {return a.price + b.price;}), 0);
   };
 
   return totalPrice;
